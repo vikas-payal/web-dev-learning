d706cc295a43fa027f21a63b3723a258671dc25e
vikas-payal <vikaspayal000@gmail.com>
2025-01-13 20:26:43 +0530

    Add files via upload

---
+++ HTML/script.js
@@ -0,0 +1,11 @@
+function changeText(){
+    var button = document.getElementById('button3');
+    button.innerHTML = "Hello World!"
+}
+
+function changeH2color(){
+    heading1 = document.getElementById("heading1");
+    heading1.style.backgroundColor = "white"
+    
+}
+ 
